466388104f14c83644a3638c8aa15aa96cef23aa
Joinmewin <39113117+Joinmewin@users.noreply.github.com>
2019-03-22 15:38:31 +0800

    Add files via upload
    APIs updated

--- example/jmjs_demo.js
+++ example/jmjs_demo.js
@@ -1,9 +1,7 @@
 
     var SetConfig = function(){
         var params = {
-
             jwt :"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE1MTA3MzEyMjQsImlhdCI6MTUxMDcyNDAyNCwidGlja2V0SWQiOiJVNGlWMzJFTHhuU1NEU2ZsVVFGeiIsInVybCI6Imh0dHBzOi8vd3d3Lmdvb2dsZS5jb20udHcvIiwiRnVuY3Rpb25MaXN0IjoiQWxsIiwiYWdpZCI6ImI0YjA0YTNiLWY5MWItNDU1Yy05N2QwLWJlYWExZGNjMzU0ZCJ9.siQvRlawE3lAlh58MV9XWxigs3yZ9XzrFUk2A42qxozUNG9wVlRm4MAQtBdS69grO297beFdarJ4K2UYfkfEBw",
-
             cb:function(err,res){
                 if(err)
                     alert(res);
@@ -13,43 +11,86 @@
         };
         jmjs.SetConfig(params);
     }
-    
-    var GetQRcode = function(){
-        var params = {
-            cb:function(err,res){
-                if(err)
-                    alert(res);
-                else
-                    alert(res);
-            }
-        };
-        jmjs.GetQRcode(params);
-    }
 
    var GetLocation = function(){
+
         var params = {
             cb:function(err,res){
                 if(err)
                     alert(res);
                 else{
-                      alert(res.latitude);
-                      alert(res.longitude);
+                      alert(JSON.stringify(res));
                 }
             }
         };
         jmjs.GetLocation(params);
     }
-    
+
+    var GetCurrentWifi = function(){
+
+        var params = {
+            cb:function(err,res){
+                if(err)
+                    alert(res);
+                else{
+                      alert(JSON.stringify(res));
+                }
+            }
+        };
+        jmjs.GetCurrentWifi(params);
+    }
+
+    var GetAllWifi = function(){
+
+        var params = {
+            cb:function(err,res){
+                if(err)
+                    alert(res);
+                else{
+                      alert(JSON.stringify(res));
+                }
+            }
+        };
+        jmjs.GetAllWifi(params);
+    }
+
+    var GetDeviceUUID = function(){
+
+        var params = {
+            cb:function(err,res){
+                if(err)
+                    alert(res);
+                else{
+                      alert(res.uuid);
+                }
+            }
+        };
+        jmjs.GetDeviceUUID(params);
+    }
+
     var GetNetworkType = function(){
         var params = {
             cb:function(err,res){
                 if(err)
                     alert(res);
                 else
-                    alert(res);
+                    alert(JSON.stringify(res));
             }
         };
         jmjs.GetNetworkType(params);
+    }
+
+
+    var GetQRcode = function(){
+        var params = {
+            cb:function(err,res){
+                if(err)
+                    alert(res);
+                else
+                    alert(JSON.stringify(res));
+            }
+        };
+        jmjs.GetQRcode(params);
     }
 
     var CloseWebView = function(){
